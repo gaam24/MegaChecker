5a52ca8d4751fd3ff6091e1d0ca9bd44056f6579
gam24 <23352901+gaam24@users.noreply.github.com>
2022-08-08 03:58:57 +0200

    Delete unnecessary directives

--- MegaChecker/messages/MessageBuilder.cs
+++ MegaChecker/messages/MessageBuilder.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
-using Colorful;
 using Console = Colorful.Console;
 
-//TODO: Change?
 namespace MegaChecker.messages
 {
     public class MessageBuilder
