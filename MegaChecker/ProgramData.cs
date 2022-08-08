b43b7190d53a2a62031a0649e109792ec54d7a8e
gam24 <23352901+gaam24@users.noreply.github.com>
2022-08-08 03:56:01 +0200

    ProgramData

---
+++ MegaChecker/ProgramData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MegaChecker.utils;
+
+namespace MegaChecker
+{
+    public static class ProgramData
+    {
+        public static string version = "beta 0.1";
+        public static string author = "gam24";
+
+        public static string console_title = string.Format("MegaChecker by {0} - {1}", author, version);
+
+        public static readonly string accounts_folder_path = "./Accounts";
+        public static readonly string backup_folder_path = "./Accounts";
+        //public static readonly string cache_folder_path = "./Accounts";
+    }
+}
