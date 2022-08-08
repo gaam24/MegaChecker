e6e9c06e54aec4171e64db5dd68cd4bb3828e08f
gam24 <23352901+gaam24@users.noreply.github.com>
2022-08-08 04:04:22 +0200

    Simple menu

--- MegaChecker/Program.cs
+++ MegaChecker/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MegaChecker.utils;
+
 namespace MegaChecker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = ProgramData.console_title;
+
+            ConsoleMenu.MainMenu();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ MegaChecker/utils/ConsoleMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+using MegaChecker.messages;
+
+namespace MegaChecker.utils
+{
+    public static class ConsoleMenu
+    {
+        public static void MainMenu()
+        {
+            MessageBuilder builder = new MessageBuilder();
+            builder.SetDefaultColor(Color.Orange);
+
+            builder.Add("Wybierz, co chcesz zrobić:");
+            builder.Add(" - 1. {0}").AddFormatter("Pokaż załadowane konta", Color.Blue);
+
+            builder.send();
+        }
+    }
+}
