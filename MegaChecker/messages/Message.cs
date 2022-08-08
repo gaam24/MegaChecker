8b2f93562cbd251a3650970baa81dd2af4d94ed8
gam24 <23352901+gaam24@users.noreply.github.com>
2022-08-08 03:56:28 +0200

    Console messages builder

---
+++ MegaChecker/messages/Message.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+
+using Colorful;
+
+namespace MegaChecker.messages
+{
+    public class Message
+    {
+        private string text;
+        private Color color;
+        private Formatter[] formatters;
+
+        public Message(string text = "Textn't")
+        {
+            this.text = text;
+            this.color = Color.White;
+            formatters = null;
+        }
+
+        public string GetText()
+        {
+            return text;
+        }
+
+        public Message SetText(string text)
+        {
+            this.text = text;
+            return this;
+        }
+
+        public Color GetColor()
+        {
+            return color;
+        }
+
+        public Message SetColor(Color color)
+        {
+            this.color = color;
+            return this;
+        }
+
+        public Formatter[] GetFormatters()
+        {
+            return formatters;
+        }
+
+        public Message SetFormatters(Formatter[] formatters)
+        {
+            this.formatters = formatters;
+            return this;
+        }
+
+        public Message AddFormatter(object target, Color color)
+        {
+            if (formatters == null) formatters = new Formatter[] { };
+
+            List<Formatter> frs = formatters.ToList();
+            frs.Add(new Formatter(target, color));
+
+            formatters = frs.ToArray();
+
+            return this;
+        }
+    }
+}
---
+++ MegaChecker/messages/MessageBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+using Colorful;
+using Console = Colorful.Console;
+
+//TODO: Change?
+namespace MegaChecker.messages
+{
+    public class MessageBuilder
+    {
+        private List<Message> messages = new List<Message>();
+        private Color default_color;
+
+        public MessageBuilder SetDefaultColor(Color color)
+        {
+            default_color = color;
+            return this;
+        }
+
+        public Message Add(string text = "Textn't")
+        {
+            Message message = new Message(text);
+            if (default_color != null) message.SetColor(default_color);
+
+            messages.Add(message);
+            return messages.Last();
+        }
+
+        public void send()
+        {
+            foreach (Message message in messages)
+            {
+                if (message.GetFormatters() != null && message.GetFormatters().Length > 0)
+                {
+                    Console.WriteLineFormatted(message.GetText(), message.GetColor(), message.GetFormatters());
+                }
+                else
+                {
+                    Console.WriteLine(message.GetText(), message.GetColor());
+                }
+            }
+        }
+    }
+}
